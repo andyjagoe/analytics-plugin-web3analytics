8225d4ea72c31b70fce451b361f3315584894095
Andy Jagoe <andy@jagoe.com>
2022-06-13 13:43:47 -0700

    webpack test in progress

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+    resolve: {
+        fallback: {
+            crypto: require.resolve("crypto-browserify"),
+            stream: require.resolve("stream-browserify"),
+            assert: require.resolve("assert"),
+            http: require.resolve("stream-http"),
+            https: require.resolve("https-browserify"),
+            os: require.resolve("os-browserify"),
+            url: require.resolve("url"),
+            path: require.resolve("path-browserify"),
+            zlib: require.resolve("browserify-zlib"),
+            fs: require.resolve("browserify-fs"),
+        }
+    }
+};
